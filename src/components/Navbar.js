725e64acab5aad5b611aaae3cc588857d88fcf5e
Sukumu <48925529+Sukumu@users.noreply.github.com>
2019-07-10 09:21:33 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Sukumu" style={{ width: '88px', height: '200px' }} />
+              <img src={logo} alt="Sukumu" style={{ width: '88px', height: '500px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
