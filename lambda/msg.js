bd355b8ecdf74088ef3ed0768533d87fb04ba0a7
Sukumu <48925529+Sukumu@users.noreply.github.com>
2019-07-16 15:05:23 -0500

    Create msg.js

---
+++ lambda/msg.js
@@ -0,0 +1,19 @@
+export function handler(event, context, callback) {
+
+  console.log('queryStringParameters', event.queryStringParameters)
+
+  callback(null, {
+
+    // return null to show no errors
+
+    statusCode: 200, // http status code
+
+    body: JSON.stringify({
+
+      msg: 'Sukumu Message: ' + Math.round(Math.random() * 30),
+
+    }),
+
+  })
+
+}
