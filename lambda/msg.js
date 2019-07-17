83e957cc57f82d299d539abb9b7664b85e2c53cd
Sukumu <48925529+Sukumu@users.noreply.github.com>
2019-07-17 07:56:55 -0500

    Update msg.js

--- lambda/msg.js
+++ lambda/msg.js
@@ -1,13 +1,62 @@
-import fetch from "node-fetch";
+! function(e, r) {
+    for (var t in r) e[t] = r[t]
+}(exports, function(e) {
+    var r = {};
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+    function t(n) {
+        if (r[n]) return r[n].exports;
+        var o = r[n] = {
+            i: n,
+            l: !1,
+            exports: {}
+        };
+        return e[n].call(o.exports, o, o.exports, t), o.l = !0, o.exports
+    }
+    return t.m = e, t.c = r, t.d = function(e, r, n) {
+        t.o(e, r) || Object.defineProperty(e, r, {
+            enumerable: !0,
+            get: n
+        })
+    }, t.r = function(e) {
+        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+            value: "Module"
+        }), Object.defineProperty(e, "__esModule", {
+            value: !0
+        })
+    }, t.t = function(e, r) {
+        if (1 & r && (e = t(e)), 8 & r) return e;
+        if (4 & r && "object" == typeof e && e && e.__esModule) return e;
+        var n = Object.create(null);
+        if (t.r(n), Object.defineProperty(n, "default", {
+                enumerable: !0,
+                value: e
+            }), 2 & r && "string" != typeof e)
+            for (var o in e) t.d(n, o, function(r) {
+                return e[r]
+            }.bind(null, o));
+        return n
+    }, t.n = function(e) {
+        var r = e && e.__esModule ? function() {
+            return e.default
+        } : function() {
+            return e
+        };
+        return t.d(r, "a", r), r
+    }, t.o = function(e, r) {
+        return Object.prototype.hasOwnProperty.call(e, r)
+    }, t.p = "", t(t.s = 0)
+}([function(e, r, t) {
+    "use strict";
 
-exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: data.joke
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
-};
+    function n(e, r, t) {
+        console.log("queryStringParameters", e.queryStringParameters), t(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+                msg: "Sukumu is King of the World! " + Math.round(.618 * Math.random())
+            })
+        })
+    }
+    t.r(r), t.d(r, "handler", function() {
+        return n
+    })
+}]));
