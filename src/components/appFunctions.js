8661f7d417e1145ef889c8aab0d60dccf9c3500a
Sukumu <48925529+Sukumu@users.noreply.github.com>
2019-09-27 13:53:25 -0500

    Update appFunctions.js

--- src/components/appFunctions.js
+++ src/components/appFunctions.js
@@ -21,3 +21,43 @@ ac_add_link('https://cdn.anychart.com/releases/v8/fonts/css/anychart-font.min.cs
 ac_add_style(document.getElementById("SukumuStyle").innerHTML);
 ac_add_style(".SukumuDiv{width:600px;height:450px;}");
 })();
+
+anychart.onDocumentReady(function () {
+
+//**********************************************************************
+var api_key = "demo";    
+var data_type = "csv";
+var FXfunction =  "FX_DAILY";    //document.getElementById("Forex_Function").innerHTML; //FunctionObj.options[FunctionObj.selectedIndex].text; 
+var from_symbol =  "EUR";   //str.substring(0, str.indexOf("/"));
+var to_symbol = "USD";    //str.substring(lastSlash+1)
+var pair = from_symbol + to_symbol;
+var today = new Date();
+var getCSV = "https://www.alphavantage.co/query?"+"function="+FXfunction+"&from_symbol="+from_symbol+"&to_symbol="+to_symbol+"&apikey="+api_key+"&datatype="+data_type;
+//**********************************************************************
+
+//anychart.data.loadCsvFile(getCSV, function (data) {
+     var dataTable = anychart.data.table();
+     dataTable.addData(get_csco_daily_short_data());
+      //dataTable.addData(data);
+      
+    //var mapping = dataTable.mapAs({'open': 1, 'high': 2, 'low': 3, 'close': 4});
+    var mapping = dataTable.mapAs({"value": 1, "value": 2, "value": 3, "value": 4});  
+    var chart = anychart.stock(); 
+    plot = chart.plot(0);  
+    var series = plot.line(mapping);
+    series.name(pair);  
+    chart.title(FXfunction);
+    chart.container("container"); 
+    chart.draw();
+
+    // reset the select list to the first option
+    chart.listen("annotationDrawingFinish", function(){document.getElementById("typeSelect").value = "default";});
+
+});
+
+// create annotations
+function create() {var select = document.getElementById("typeSelect");
+  plot.annotations().startDrawing(select.value);}
+
+// remove all annotations
+function removeAll() {plot.annotations().removeAllAnnotations();}
