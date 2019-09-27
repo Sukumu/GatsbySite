de3913f6a54eaff5d98a814f0b19a8c78837ae46
Sukumu <48925529+Sukumu@users.noreply.github.com>
2019-09-27 13:42:29 -0500

    Update product-page.js

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -48,33 +48,10 @@ export const ProductPageTemplate = ({
 <script src="https://cdn.anychart.com/releases/8.7.0/js/anychart-exports.min.js"></script>
 <script src="https://cdn.anychart.com/releases/8.7.0/js/anychart-ui.min.js"></script>
 <script src="https://cdn.anychart.com/releases/8.7.0/js/anychart-data-adapter.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/Sukumu/GatsbySite@latest/src/components/appFunctions.js"></script>
 <script src="https://cdn.anychart.com/csv-data/csco-daily-short.js"></script>
 <div id="SukumuStyle" style="display:none;">
-
 </div>
-
-<script>(function(){
-function ac_add_to_head(el){
-	var head = document.getElementsByTagName('head')[0];
-	head.insertBefore(el,head.firstChild);
-}
-function ac_add_link(url){
-	var el = document.createElement('link');
-	el.rel='stylesheet';el.type='text/css';el.media='all';el.href=url;
-	ac_add_to_head(el);
-}
-function ac_add_style(css){
-	var ac_style = document.createElement('style');
-	if (ac_style.styleSheet) ac_style.styleSheet.cssText = css;
-	else ac_style.appendChild(document.createTextNode(css));
-	ac_add_to_head(ac_style);
-}
-ac_add_link('https://cdn.jsdelivr.net/gh/Sukumu/GatsbySite@latest/src/components/all.sass');
-ac_add_link('https://cdn.anychart.com/releases/8.7.0/css/anychart-ui.min.css');
-ac_add_link('https://cdn.anychart.com/releases/v8/fonts/css/anychart-font.min.css');
-ac_add_style(document.getElementById("SukumuStyle").innerHTML);
-ac_add_style(".SukumuDiv{width:600px;height:450px;}");
-})();</script>
 
 <select id="typeSelect" onclick="create()">
     <option value="default" selected disabled>Annotation Type</option>
